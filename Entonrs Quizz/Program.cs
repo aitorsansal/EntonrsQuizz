3680effc349663eb4e5935f7a8f5bffa87d1b51b
Manuel Soto <a2308926@institutmontilivi.cat>
2024-02-08 17:22:46 +0100

    tercer commit

--- Entonrs Quizz/Program.cs
+++ Entonrs Quizz/Program.cs
@@ -29,6 +29,8 @@ internal class Program
     static void CreateManuTopics()
     {
         DragonBallTopics();
+        FutbolTopics();
+        CulturaGeneralTopics();
     }
     static void DragonBallTopics()
     {
@@ -47,5 +49,41 @@ internal class Program
             ];
         Topic DragonBall = new Topic("Dragon Ball", PreguntasDragonBall);
         topics.Add(DragonBall);
+    }
+    static void FutbolTopics()
+    {
+        Question primera = new Question("¿Cuándo empezó LaLiga?", "20-04-1930", "10-02-1929", "20-02-1920", "01-03-1940", "10-02-1929");
+        Question segunda = new Question("¿Quién es el jugador que ha logrado el trofeo Pichichi en más ocasiones?", "Leo Messi", "Cristiano Ronaldo", "Iniesta", "Xavi", "Leo Messi");
+        Question tercera = new Question("¿Quién es el entrenador que ha dirigido durante más temporadas al Atlético de Madrid?", "Mourinho", "Cholo Simeone", "Luis Aragonés", "Pep Guardiola", "Luis Aragonés");
+        Question quarta = new Question("¿Cuántas temporadas de Liga se han disputado hasta el día de hoy?", "98", "70", "100", "91", "91");
+        Question quinta = new Question("¿Quién ha sido el último Pichichi de LaLiga?", "Bencema", "Leo Messi", "Iniesta", "Levandowski", "Bencema");
+        List<Question> PreguntasFutbol =
+            [
+                primera,
+                segunda,
+                tercera,
+                quarta,
+                quinta
+            ];
+        Topic Futbol = new Topic("Futbol", PreguntasFutbol);
+        topics.Add(Futbol);
+    }
+    static void CulturaGeneralTopics()
+    {
+        Question primera = new Question("¿Cuál es el río más largo del mundo?", "Amazonas", "Duero", "Tajo", "Oñar", "Amazonas");
+        Question segunda = new Question("¿Dónde está Transilvania?", "España", "Brasil", "Irlanda", "Rumania", "Rumania");
+        Question tercera = new Question("¿Cuántos años duró la Primera Guerra Mundial?", "4", "2", "6", "5", "4");
+        Question quarta = new Question("¿Qué año llegó Cristóbal Colón a América?", "1942", "1956", "1926", "1938", "1942");
+        Question quinta = new Question("¿Cuántos huesos tiene el cuerpo humano?", "70", "195", "206", "208", "206");
+        List<Question> PreguntasCulturaGeneral =
+            [
+                primera,
+                segunda,
+                tercera,
+                quarta,
+                quinta
+            ];
+        Topic CulturaGeneral = new Topic("Cultura General", PreguntasCulturaGeneral);
+        topics.Add(CulturaGeneral);
     }
 }
